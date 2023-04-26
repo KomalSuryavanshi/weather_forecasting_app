e1f9b08d96f6e450ac90e74e6971fe6037d26a2b
Komal <komalwork11@gmail.com>
2023-04-26 14:12:19 +0530

    Final Commit

--- backend.py
+++ backend.py
@@ -3,12 +3,17 @@ import requests
 API_KEY = "06486787f1cb5505bfa4c3d1855c6b3e"
 
 
-def get_data(place, forecast_days=None, type_of_weather=None):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     content = response.json()
-    return content
+
+    # filtering data according to forecast days
+    filtered_content = content["list"]
+    no_of_values = 8 * forecast_days  # 8 because 8 observations are seen in 24 hours so 8 * no of forecast days
+    filtered_content = filtered_content[:no_of_values]
+    return filtered_content
 
 
 if __name__ == "__main__":
-    print(get_data(place="London"))
+    print(get_data(place="London",forecast_days=3))
--- main.py
+++ main.py
@@ -10,7 +10,23 @@ option = st.selectbox("Select the data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:  # if place is provided on then call the function
+    # Get the temperature/ sky
+    try:
+        filtered_content = backend.get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_content]  # dict is a variable
+            dates = [dict["dt_txt"] for dict in filtered_content]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_content]  # 1. Sky is stored under main
+            # variable 2. [0] because weather is dictionary with only one entity in which main or sky is mentioned
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.write("Entered Place Does Not Exists !!")
